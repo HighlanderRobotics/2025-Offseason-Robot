24f569894ec06cc420fc6e2374f192f1efb1c59d
vivi-o <vivie@oesterer.com>
2025-08-13 00:10:17 -0700

    estimating arm state values

--- src/main/java/frc/robot/arm/ArmSubsystem.java
+++ src/main/java/frc/robot/arm/ArmSubsystem.java
@@ -30,27 +30,27 @@ public class ArmSubsystem extends SubsystemBase {
   public enum ArmState {
     IDLE(Rotation2d.fromDegrees(0), 0.0),
     // coral
-    READY_CORAL(Rotation2d.fromDegrees(0), 0.0),
-    PRE_INTAKE_CORAL_GROUND(Rotation2d.fromDegrees(0), 0.0),
-    INTAKE_CORAL_GROUND(Rotation2d.fromDegrees(0), 0.0),
-    L1(Rotation2d.fromDegrees(0), 10.0),
-    PRE_L2(Rotation2d.fromDegrees(0), 10.0),
-    L2(Rotation2d.fromDegrees(0), 10.0),
-    PRE_L3(Rotation2d.fromDegrees(0), 10.0),
-    L3(Rotation2d.fromDegrees(0), 10.0),
-    PRE_L4(Rotation2d.fromDegrees(0), 10.0),
-    L4(Rotation2d.fromDegrees(0), 10.0),
-    POST_L4(Rotation2d.fromDegrees(0), 0.0),
+    READY_CORAL(Rotation2d.fromDegrees(180), 0.0),
+    PRE_INTAKE_CORAL_GROUND(Rotation2d.fromDegrees(180), 0.0),
+    INTAKE_CORAL_GROUND(Rotation2d.fromDegrees(180), 0.0),
+    L1(Rotation2d.fromDegrees(100), 10.0),
+    PRE_L2(Rotation2d.fromDegrees(45), 10.0),
+    L2(Rotation2d.fromDegrees(60), 10.0),
+    PRE_L3(Rotation2d.fromDegrees(45), 10.0),
+    L3(Rotation2d.fromDegrees(70), 10.0),
+    PRE_L4(Rotation2d.fromDegrees(60), 10.0),
+    L4(Rotation2d.fromDegrees(90), 10.0),
+    POST_L4(Rotation2d.fromDegrees(90), 0.0),
     // algae
-    INTAKE_ALGAE_REEF_HIGH(Rotation2d.fromDegrees(0), 0.0),
-    INTAKE_ALGAE_REEF_LOW(Rotation2d.fromDegrees(0), 0.0),
-    INTAKE_ALGAE_GROUND(Rotation2d.fromDegrees(0), 0.0),
-    BARGE(Rotation2d.fromDegrees(0), 0.0),
+    INTAKE_ALGAE_REEF_HIGH(Rotation2d.fromDegrees(90), 0.0),
+    INTAKE_ALGAE_REEF_LOW(Rotation2d.fromDegrees(90), 0.0),
+    INTAKE_ALGAE_GROUND(Rotation2d.fromDegrees(135), 0.0),
+    BARGE(Rotation2d.fromDegrees(30), 0.0),
     READY_ALGAE(Rotation2d.fromDegrees(0), 0.0),
-    PROCESSOR(Rotation2d.fromDegrees(0), 0.0),
+    PROCESSOR(Rotation2d.fromDegrees(90), 0.0),
     // climbing
     PRE_CLIMB(Rotation2d.fromDegrees(0), 0.0),
-    CLIMB(Rotation2d.fromDegrees(0), 0.0);
+    CLIMB(Rotation2d.fromDegrees(20), 0.0);
 
     public final Rotation2d position;
     public final double volts;
