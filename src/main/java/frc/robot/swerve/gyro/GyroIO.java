f9778aaa1576f25633db80b061beed6dc54b11bc
SCool62 <samthecool300@icloud.com>
2025-08-21 09:40:34 -0700

    Add gyro

---
+++ src/main/java/frc/robot/swerve/gyro/GyroIO.java
@@ -0,0 +1,23 @@
+package frc.robot.swerve.gyro;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public interface GyroIO {
+
+    @AutoLog
+    public static class GyroIOInputs {
+        public Rotation2d yaw = new Rotation2d();
+        public double yawVelocityRadPerSec = 0.0;
+        public Rotation2d pitch = new Rotation2d();
+        public Rotation2d roll = new Rotation2d();
+
+        public boolean isConnected = false;
+    }
+
+    public void updateInputs(GyroIOInputs inputs);
+
+
+    public void setYaw(Rotation2d yaw);
+}
