c0243b242581e2be5e1c48fa323d56bf77430400
spellingcat <70864274+spellingcat@users.noreply.github.com>
2025-10-04 21:15:18 -0700

    add climber state lol

--- src/main/java/frc/robot/Superstructure.java
+++ src/main/java/frc/robot/Superstructure.java
@@ -72,8 +72,8 @@ public class Superstructure {
     PRE_PROCESSOR(ElevatorState.PROCESSOR, ArmState.PRE_PROCESSOR, IntakeState.IDLE),
     SCORE_PROCESSOR(ElevatorState.PROCESSOR, ArmState.SCORE_PROCESSOR, IntakeState.IDLE),
 
-    PRE_CLIMB(ElevatorState.PRE_CLIMB, ArmState.PRE_CLIMB, IntakeState.CLIMB),
-    CLIMB(ElevatorState.CLIMB, ArmState.CLIMB, IntakeState.CLIMB);
+    PRE_CLIMB(ElevatorState.PRE_CLIMB, ArmState.PRE_CLIMB, IntakeState.CLIMB, ClimberState.PRE_CLIMB),
+    CLIMB(ElevatorState.CLIMB, ArmState.CLIMB, IntakeState.CLIMB, ClimberState.CLIMB);
 
     public final ElevatorState elevatorState;
     public final ArmState armState;
