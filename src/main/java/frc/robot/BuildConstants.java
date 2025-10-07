52ab3bf9a84327e7134ae5a1513e2bfcadf0562a
spellingcat <70864274+spellingcat@users.noreply.github.com>
2025-10-06 17:20:41 -0700

    rename folder

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2025-Offseason-Robot";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 16;
+  public static final String GIT_SHA = "c9b33c91bdee9c715fb27441c43edfd4d04ec38d";
+  public static final String GIT_DATE = "2025-10-04 13:44:21 PDT";
+  public static final String GIT_BRANCH = "mttd-superstructure";
+  public static final String BUILD_DATE = "2025-10-04 13:50:03 PDT";
+  public static final long BUILD_UNIX_TIME = 1759611003777L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
