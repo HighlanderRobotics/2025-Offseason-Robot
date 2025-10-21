fa9dd1acc61b1ccc158b463371d67c1ab0d77aab
SCool62 <79726681+SCool62@users.noreply.github.com>
2025-10-21 00:09:48 +0000

    run spotless

--- src/main/java/frc/robot/swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/swerve/SwerveSubsystem.java
@@ -23,7 +23,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
-import frc.robot.Robot.CoralScoreTarget;
 import frc.robot.Robot.RobotType;
 import frc.robot.camera.Camera;
 import frc.robot.camera.CameraIOReal;
