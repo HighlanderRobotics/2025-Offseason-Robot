605b1beab5f2ce589bed71252c6c6f4105b3d399
spellingcat <70864274+spellingcat@users.noreply.github.com>
2025-11-08 00:12:33 -0800

    hopefully add correct led port but like no one knows for some reason

--- src/main/java/frc/robot/led/LEDSubsystem.java
+++ src/main/java/frc/robot/led/LEDSubsystem.java
@@ -13,8 +13,8 @@ import java.util.function.Supplier;
 import org.littletonrobotics.junction.Logger;
 
 public class LEDSubsystem extends SubsystemBase {
-  public static final int LED_LENGTH = 25;
-  public static final int LED_ID = 2;
+  public static final int LED_LENGTH = 30; //no one knows what this number is lmaoooo
+  public static final int LED_ID = 0;
 
   public static final Color PURPLE = new Color("#A000D0");
 
